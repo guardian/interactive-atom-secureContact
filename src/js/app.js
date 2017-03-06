995062178e2280c76a4a3107c4a6f7c542c50c76
cfen <chris.fenn@guardian.co.uk>
2017-03-06 16:18:59 +0000

    added mobile styling

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 console.log("app running");
 
+var cardsArr;
+
 function init() {
     addListeners();
 }
@@ -13,7 +15,7 @@ function addListeners() {
 
     [].slice.apply(document.querySelectorAll('.nav-one-item')).forEach(el => {
         //var data = el.className.replace('share-','');
-        el.addEventListener('click', () => clickActivateNavItem(el));
+        el.addEventListener('click', () => toggleActiveNavItem(el));
     });
 }
 
@@ -23,11 +25,11 @@ function clickReadMore(a) {
 
     [].slice.apply(document.querySelectorAll('.gv-read-more')).forEach(c => {
         let d = c.getAttribute("div-ref");
-        if (b == d) { clickActivate(c) }
+        if (b == d) { toggleActive(c) }
     });
 }
 
-function clickActivate(a) {
+function toggleActive(a) {
 
     if (a.classList.contains("active")) {
         a.classList.remove("active");
@@ -46,39 +48,125 @@ function clickActivate(a) {
 }
 
 
-function clickActivateNavItem(a){
+function toggleActiveNavItem(a) {
     let b = a.getAttribute("nav-level");
 
-    if (b == "L1") { 
-            [].slice.apply(document.querySelectorAll('.nav-one-item')).forEach(el => {
+    if (b == "L1") {
+        [].slice.apply(document.querySelectorAll('.nav-one-item')).forEach(el => {
             el.classList.remove("selected");
             el.classList.add("unselected");
         });
         a.classList.remove("unselected");
         a.classList.add("selected");
+        document.querySelector('.nav-step-two').classList.remove("inactive");
+        document.querySelector('.nav-step-three').classList.add("inactive");
     }
 
-    if (b == "L2") { 
-            [].slice.apply(document.querySelectorAll('.nav-one-item')).forEach(el => {
+    if (b == "L2") {
+        [].slice.apply(document.querySelectorAll('.nav-one-item')).forEach(el => {
 
             let c = el.getAttribute("nav-level");
 
 
-                
+
             if (c == b) {
                 el.classList.remove("selected");
                 el.classList.add("unselected");
-            }   
-            
+            }
+
         });
-        console.log(a.getAttribute("target-cards")) 
+        getCardsArr(a.getAttribute("target-cards"))
         a.classList.remove("unselected");
         a.classList.add("selected");
+
+        document.querySelector('.nav-step-three').classList.remove("inactive");
     }
 
 
 }
 
+function getCardsArr(a) {
+    var b = a.split("---");
+
+    var c = [];
+
+    for (var d = 0; d < b.length; d++) {
+        if (b[d]) {
+            c.push(b[d]);
+        }
+    }
+
+    displayCards(c)
+
+}
+
+function displayCards(a) {
+
+    let b = document.querySelectorAll(".gv-card");
+
+    for (var d = 0; d < b.length; d++) {
+        b[d].classList.remove("active");
+        b[d].classList.add("inactive");
+    }
+
+    for (var c = 0; c < a.length; c++) {
+        console.log(a[c]);
+        for (var d = 0; d < b.length; d++) {
+            //console.log(b[d])
+
+            if (a[c] == b[d].getAttribute("card-ref")) {
+                console.log(b[d])
+
+                b[d].classList.remove("inactive");
+                b[d].classList.add("active");
+
+            }
+
+
+        }
+
+
+
+    }
+
+    displayPips(a)
+
+}
+
+
+function displayPips(a) {
+
+    let b = document.querySelectorAll(".gv-chiclet");
+
+    for (var d = 0; d < b.length; d++) {
+        b[d].classList.remove("selected");
+        b[d].classList.add("unselected");
+    }
+
+
+    for (var c = 0; c < a.length; c++) {
+        console.log(a[c]);
+        for (var d = 0; d < b.length; d++) {
+            //console.log(b[d])
+
+            if (a[c] == b[d].getAttribute("card-ref")) {
+                console.log(b[d])
+
+                b[d].classList.remove("unselected");
+                b[d].classList.add("selected");
+
+            }
+
+
+        }
+
+
+
+    }
+
+
+
+}
 
 
 init();
