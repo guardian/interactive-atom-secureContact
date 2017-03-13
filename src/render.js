f6dd3577fd81436cbf7ad268be1b7dae51678d10
cfen <chris.fenn@guardian.co.uk>
2017-03-13 18:14:28 +0000

    Trying out auto whitespace fix

--- src/render.js
+++ src/render.js
@@ -1,22 +1,37 @@
 import mainTemplate from './src/templates/main.html!text'
+import mainEdit from './src/templates/mainEdit.html!text'
 import Mustache from 'mustache'
 import rp from 'request-promise'
 
-export function render() {
-    return rp({
-        uri: 'https://interactive.guim.co.uk/docsdata-test/1WLhHROz2EB2rW25ouywEmw_u-Bi_9W7UDoVlxTHy1Zs.json',
-        json: true
-    }).then((data) => {
-	      return buildView(data)
-    });
+export async function render() {
 
-   
+	let data = formatData(await rp({
+        uri: "https://interactive.guim.co.uk/docsdata-test/1WLhHROz2EB2rW25ouywEmw_u-Bi_9W7UDoVlxTHy1Zs.json",
+        json: true
+    }));
+
+    return buildEditView(data)
+
+}
+
+function buildEditView(d){
+	let editHtml = Mustache.render(mainEdit, d );
+    return `${editHtml}`;
 }
 
 
 
-function buildView(data){
-	var html = Mustache.render(mainTemplate, data);
+function formatData(data) {
+    data.cards.map((card) => {
+        card.formattedReadMore = whitespaceFix(card);
+        return card;
+    });
 
-    return html;
+    return data;
+}
+
+
+function whitespaceFix(card) {
+    let text = card.readMoreCopy.replace(/\t\r/g, "").replace(/\n\s*\n/g, '</p><p>');
+    return `<p>${text}</p>`
 }
